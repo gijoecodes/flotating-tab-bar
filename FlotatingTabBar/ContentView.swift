97be6084c25f1f820e84bc040ba8edccaad4c44c
GIJoeCodes <31097875+gijoecodes@users.noreply.github.com>
2024-09-30 13:28:07 -0400

    created xcode project

---
+++ FlotatingTabBar/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  FlotatingTabBar
+//
+//  Created by Jose Carrillo on 9/30/24.
+//  Built using Swift 5.0
+//
+//
+//  Redistribution or use in source and binary forms, with or without
+//  modification, are strictly prohibited without explicit written permission
+//  from the copyright holder.
+//
+//  You may not use, copy, modify, merge, publish, distribute, sublicense,
+//  create a derivative work, and/or sell copies of the software in any work
+//  that is designed, intended, or marketed for pedagogical or instructional
+//  purposes related to programming, coding, application development, or
+//  information technology. Permission for such use, copying, modification,
+//  merger, publication, distribution, sublicensing, creation of derivative works,
+//  or sale is expressly withheld.
+//
+//  Copyright © 2024 Jose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
